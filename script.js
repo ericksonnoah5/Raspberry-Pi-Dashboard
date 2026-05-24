f285d8236e485be9b60093f8d4c2c43aa820e5b6
Noah Erickson <ericksonnoah5@gmail.com>
2026-05-24 14:05:26 -0500

    day2

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
+var tempDoc = document.getElementById("temp");
+var windDoc = document.getElementById("wind");
+var dateDoc = document.getElementById("date");
+
 var tempC;
-var temp = document.getElementById("temp");
+var windspeed;
+var date;
 
-fetch(
-  "https://api.open-meteo.com/v1/forecast?latitude=41.7317&longitude=-93.6001&current_weather=true",
-)
-  .then((response) => {
-    if (!response.ok) throw new Error("Network response was not ok");
-    return response.json();
-  })
-  .then((data) => {
-    tempC = data.current_weather.temperature;
-    var tempF;
+onStart();
+setInterval(() => {
+  loadtemp();
+}, 6000);
 
-    tempF = (tempC * 9) / 5 + 32;
-    console.log(tempF);
-    temp.innerHTML = tempF;
-  })
-  .catch((error) => {
-    console.error("Fetch error:", error);
-  });
+function onStart() {
+  loadtemp();
+}
+
+function loadtemp() {
+  fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=41.7317&longitude=-93.6001&current_weather=true",
+  )
+    .then((response) => {
+      if (!response.ok) throw new Error("Network response was not ok");
+      return response.json();
+    })
+    .then((data) => {
+      tempC = data.current_weather.temperature;
+      windspeed = data.current_weather.windspeed;
+      date = data.current_weather.time.substring(0, 9);
+
+      var tempF;
+
+      tempF = (tempC * 9) / 5 + 32;
+
+      windspeed = windspeed * 0.621371;
+      tempDoc.innerHTML = tempF + "\u00B0";
+      windDoc.innerHTML = windspeed + " MPH";
+      dateDoc.innerHTML = date;
+    })
+
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+}
