a16de3782fd719f31759ce66b9440fe04909a4fb
Noah Erickson <ericksonnoah5@gmail.com>
2026-06-06 11:45:05 -0500

    photos and more

--- script.js
+++ script.js
@@ -2,27 +2,43 @@ var tempDoc = document.getElementById("temp");
 var windDoc = document.getElementById("wind");
 var dateDoc = document.getElementById("date");
 var timeDoc = document.getElementById("time");
-var imgDoc = document.getElementById("img");
+var imgA = document.getElementById("img");
+var imgB = document.getElementById("img2");
+var forecastDoc = document.getElementById("forecast");
 
-var time;
-var tempC;
-var windspeed;
-var date;
-var count = 0;
+var photos = [
+  "./photos/img.JPG",
+  "./photos/img2.jpeg",
+  "./photos/img4.PNG",
+  "./photos/img5.jpeg",
+  "./photos/img6.png",
+  "./photos/img7.jpeg",
+];
+var photoIndex = 0;
+var activeLayer = imgA;
+
+function weatherEmoji(code) {
+  if (code === 0) return "☀️";
+  if (code <= 2) return "🌤️";
+  if (code === 3) return "☁️";
+  if (code <= 48) return "🌫️";
+  if (code <= 55) return "🌦️";
+  if (code <= 65) return "🌧️";
+  if (code <= 77) return "🌨️";
+  if (code <= 82) return "🌦️";
+  if (code <= 86) return "🌨️";
+  return "⛈️";
+}
+
+function cToF(c) {
+  return Math.round((c * 9) / 5 + 32);
+}
 
 onStart();
+
 setInterval(() => {
   loadtemp();
-  gettime();
-
-  count += 1;
-  if (count % 3 == 0) {
-    imgDoc.src = "./photos/img.JPG";
-  } else if (count % 3 == 1) {
-    imgDoc.src = "./photos/img2.jpeg";
-  } else {
-    imgDoc.src = "./photos/img3.PNG";
-  }
+  advancePhoto();
 }, 60000);
 
 setInterval(() => {
@@ -34,35 +50,72 @@ function onStart() {
   gettime();
 }
 
+function advancePhoto() {
+  photoIndex = (photoIndex + 1) % photos.length;
+  var nextLayer = activeLayer === imgA ? imgB : imgA;
+
+  nextLayer.src = photos[photoIndex];
+  nextLayer.onload = () => {
+    nextLayer.classList.add("active");
+    activeLayer.classList.remove("active");
+    activeLayer = nextLayer;
+  };
+}
+
 function loadtemp() {
   fetch(
-    "https://api.open-meteo.com/v1/forecast?latitude=41.7317&longitude=-93.6001&current_weather=true",
+    "https://api.open-meteo.com/v1/forecast?latitude=41.7317&longitude=-93.6001" +
+      "&current_weather=true" +
+      "&daily=temperature_2m_max,temperature_2m_min,weathercode" +
+      "&timezone=America/Chicago&forecast_days=7",
   )
     .then((response) => {
       if (!response.ok) throw new Error("Network response was not ok");
       return response.json();
     })
     .then((data) => {
-      tempC = data.current_weather.temperature;
-      windspeed = data.current_weather.windspeed;
-      date = data.current_weather.time.substring(0, 10);
+      var tempF = cToF(data.current_weather.temperature);
+      var windKph = data.current_weather.windspeed;
+      var windMph = parseInt(windKph * 0.621371);
 
-      var tempF;
+      tempDoc.innerHTML = tempF + "°";
+      windDoc.innerHTML = windMph + " mph Wind";
 
-      tempF = (tempC * 9) / 5 + 32;
-
-      windspeed = windspeed * 0.621371;
-      tempDoc.innerHTML = tempF + "\u00B0";
-      windDoc.innerHTML = parseInt(windspeed) + " mph Wind";
+      renderForecast(data.daily);
     })
-
     .catch((error) => {
       console.error("Fetch error:", error);
     });
 }
 
+function renderForecast(daily) {
+  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var html = "";
+
+  for (var i = 0; i < 7; i++) {
+    var date = new Date(daily.time[i] + "T12:00:00");
+    var dayName = i === 0 ? "Today" : days[date.getDay()];
+    var emoji = weatherEmoji(daily.weathercode[i]);
+    var hi = cToF(daily.temperature_2m_max[i]);
+    var lo = cToF(daily.temperature_2m_min[i]);
+
+    html +=
+      '<div class="forecast-day">' +
+      '<span class="forecast-emoji">' + emoji + "</span>" +
+      '<span class="forecast-name">' + dayName + "</span>" +
+      '<span class="forecast-temps">' + hi + "° <span class='lo'>" + lo + "°</span></span>" +
+      "</div>";
+  }
+
+  forecastDoc.innerHTML = html;
+}
+
 function gettime() {
   const now = new Date();
-  timeDoc.innerHTML = now.toLocaleTimeString([], { hour: "numeric", minute: "2-digit" });
+  timeDoc.innerHTML = now.toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
   dateDoc.innerHTML = now.toLocaleDateString();
 }
